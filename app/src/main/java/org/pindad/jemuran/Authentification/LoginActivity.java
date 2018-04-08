d4dc544f799736508a79d05dbeb0d8495b03170d
Mirza Himami Hanif <mirzahanif7@gmail.com>
2018-04-08 18:26:43 +0700

    Fungsi Utama Fix

--- app/src/main/java/org/pindad/jemuran/Authentification/LoginActivity.java
+++ app/src/main/java/org/pindad/jemuran/Authentification/LoginActivity.java
@@ -1,6 +1,8 @@
 package org.pindad.jemuran.Authentification;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -14,9 +16,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.pindad.jemuran.MainActivity;
 import org.pindad.jemuran.R;
+import org.pindad.jemuran.Sensor.ModelSensor.ListSensor;
 
 /**
  * Created by ASUS on 22/03/2018.
@@ -26,6 +34,9 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText mUsername, mPassword;
     private Button mLogin;
+    private String username, password;
+    FirebaseDatabase database;
+    DatabaseReference myRef, myRef2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +45,16 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        mUsername = (EditText) findViewById(R.id.email);
+        mUsername = (EditText) findViewById(R.id.username);
         mPassword = (EditText) findViewById(R.id.password);
         mLogin =(Button) findViewById(R.id.btn_login);
 
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                username = mUsername.getText().toString();
+                password = mPassword.getText().toString();
+                signIn();
             }
         });
     }
@@ -49,18 +62,44 @@ public class LoginActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    private void updateUI(FirebaseUser currentUser) {
-        if(currentUser!=null){
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            finish();
+        SharedPreferences sharedPref = getSharedPreferences("Shared Preference", Context.MODE_PRIVATE);
+        if (sharedPref.getString("username", null)!=null){
+            updateUI();
         }
+
     }
 
+    private void updateUI() {
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
+    }
     private void signIn(){
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference().child(username);
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                try{
+                    String checkPassword = dataSnapshot.child("password").getValue(String.class);
+                    if (checkPassword.equals(password)){
+                        Context context = getApplicationContext();
+                        SharedPreferences sharedPref = context.getSharedPreferences(
+                                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putString(getString(R.string.username), username);
+                        editor.commit();
+                        updateUI();
+                    }
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+
+        });
     }
 }
